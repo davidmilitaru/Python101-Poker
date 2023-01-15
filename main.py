d5b23a046ad1fae68a825242ba19f947db969710
davidmilitaru <95294268+davidmilitaru@users.noreply.github.com>
2023-01-16 01:13:52 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,33 @@
 import poker as pkr
     
-def main ():
-    nr_players = eval (input ('Enter number of players: '))
-    while (nr_players < 2 or nr_players > 6):
-        nr_players = eval( input ('Enter number of players: ') )  
+nr_players = eval (input ('Enter number of players: '))
+while (nr_players < 2 or nr_players > 6):
+    nr_players = eval( input ('Enter number of players: ') )  
 
-    multiple_games_list = []
-    counter = 0
+multiple_games_list = []
+counter = 0
+for i in range(nr_players):
+    multiple_games_list.append(0)
+    counter += 1
+
+game_won = counter
+while (game_won == counter):                        #a little ambiguous, but as long as nobody hit 52 points, this while loop will not end
+    game = pkr.Poker (nr_players)
+    game.play()
+    print('\n')
     for i in range(nr_players):
-        multiple_games_list.append(0)
-        counter += 1
+        current_hand = game.hands[i]
+        print ("Player "+ str(i+1) + ": " , end="")
+        game.isRoyal(current_hand)
 
-    game_won = counter
-    while (game_won == counter):        #a little ambiguous, but as long as nobody hit 52 points, this while loop will not end
-        game = pkr.Poker (nr_players)
-        game.play()
-        print('\n')
-        for i in range(nr_players):
-            current_hand = game.hands[i]
-            print ("Player "+ str(i+1) + ": " , end="")
-            game.isRoyal(current_hand)
+    maxpoint = max(game.tlist)
+    maxindex = game.tlist.index(maxpoint)
 
-        maxpoint = max(game.tlist)
-        maxindex = game.tlist.index(maxpoint)
+    print ('\nPlayer %d wins'% (maxindex+1))
+    multiple_games_list[maxindex] += game.multiple_games_list[maxindex]
 
-        print ('\nPlayer %d wins'% (maxindex+1))
-        multiple_games_list[maxindex] += game.multiple_games_list[maxindex]
-
-        for i in range(nr_players):
-            if multiple_games_list[i] >= 52:
-                game_won -= 1           #so as to exit the while loop and declare the winner
-                print(multiple_games_list)
-                print(f"Player {maxindex + 1} has won the game!")
-  
-main()
+    for i in range(nr_players):
+        if multiple_games_list[i] >= 52:
+            game_won -= 1                           #so as to exit the while loop and declare the winner
+            print(multiple_games_list)
+            print(f"Player {maxindex + 1} has won the game!")
